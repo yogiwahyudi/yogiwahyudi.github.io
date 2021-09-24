c8d6273442b220fc2c1b032071de13c599bc71e2
yogi wahyudi <yogiwahyudi01@gmail.com>
2021-09-24 17:34:53 +0700

    Bug final project done!

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function countWord(){
     let text = textField.value;
     text = text.trim();
     const words = text.split(" ");
-    wordCount.innerHTML = words.length;
     character.innerHTML = text.length;
+    
 
     let vowels = ['a','i','u','e','o']
     let consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
@@ -18,6 +18,9 @@ function countWord(){
     let temp
     for(let j=0; j<words.length;j++){
         temp = words[j]
+        if(words[j] != ''){
+            wordCount.innerHTML = words[j].length;
+        }
         for(let i=0; i<temp.length;i++){
             for(let k=0; k<vowels.length;k++){
                 if(temp[i] === vowels[k]){
@@ -29,6 +32,7 @@ function countWord(){
                     counterConsonants++
                 }
             }
+          
         }
     }
     vowelsCount.innerHTML=counterVowels
